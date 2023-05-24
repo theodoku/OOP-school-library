5175d6dc39db278c94df599352b023a9b7ee8010
theophilus doku <theodoku89@gmail.com>
2023-05-24 18:37:46 +0000

    Add people.rb file

--- load_people.rb
+++ load_people.rb
@@ -1,28 +1,20 @@
-require_relative './person'
 require_relative './student'
 require_relative './teacher'
+require_relative './file_handler'
 
 def load_people
-  file_handler_students = FileHandler.new('./student.json')
-  file_handler_teachers = FileHandler.new('./teacher.json')
-
-  students_data = file_handler_students.read_from_file
-  teachers_data = file_handler_teachers.read_from_file
-
-  @people = []
-
-  students = students_data ? JSON.parse(students_data) : []
-  teachers = teachers_data ? JSON.parse(teachers_data) : []
-
-  students.each do |student_data|
-    student = Student.new(student_data['Name'], student_data['Age'], student_data['Classroom'])
-    student.id = student_data['ID']
-    @people << student
-  end
-
-  teachers.each do |teacher_data|
-    teacher = Teacher.new(teacher_data['Name'], teacher_data['Age'], teacher_data['Specialization'])
-    teacher.id = teacher_data['ID']
-    @people << teacher
+  @file_handler = FileHandler.new('./people.json')
+  data = @file_handler.read_from_file
+  @save_people = JSON.parse(data)
+  @save_people.each do |person_data|
+    if person_data['Type'] == 'Student'
+      student = Student.new(person_data['Name'], person_data['Age'], person_data['Classroom'])
+      student.id = person_data['ID']
+      @people << student
+    elsif person_data['Type'] == 'Teacher'
+      teacher = Teacher.new(person_data['Name'], person_data['Age'], person_data['Specialization'])
+      teacher.id = person_data['ID']
+      @people << teacher
+    end
   end
 end
