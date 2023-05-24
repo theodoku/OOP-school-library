7dc14d3f5ab14f8bd7c66af050b73f00f854255d
theophilus doku <theodoku89@gmail.com>
2023-05-24 11:17:02 +0000

    Modify file_handler.rb file

--- file_handler.rb
+++ file_handler.rb
@@ -1,14 +1,25 @@
 require 'json'
-class File_Handler
-    attr_accessor :path, :store
-    def initialize(path)
-        @path = path
-    end
+class FileHandler
+  attr_accessor :path, :store
 
-    def write_to_file(data)
-        file = File.open(@path, 'w')
-        # @store << data
-        file.write(data)
-        file.close
-    end
+  def initialize(path)
+    @path = path
+  end
+
+  def write_to_file(data)
+    file = File.open(@path, 'w')
+    # @store << data
+    file.write(data)
+    file.close
+  end
+
+  def read_from_file
+    return nil unless File.exist?(@path)
+
+    file = File.open(@path, 'r')
+    data = file.read
+    file.close
+
+    data
+  end
 end
