f6d02436b096973276a40ecc547522288d0b2856
Caleb Nwaizu <calebchris000@gmail.com>
2023-05-24 21:27:00 +0100

    Fix error on app data overwriting on new rewrite

--- load_books.rb
+++ load_books.rb
@@ -1,13 +1,12 @@
 require_relative './book'
 
 def load_books
-  file_handler = FileHandler.new('./book.json')
-  data = file_handler.read_from_file
-
+  @file_handler = FileHandler.new('./book.json')
+  data = @file_handler.read_from_file
   return if data.nil?
+  @save_books = JSON.parse(data)
 
-  books_data = JSON.parse(data)
-  books_data.each do |book_data|
+  @save_books.each do |book_data|
     book = Book.new(book_data['Title'], book_data['Author'], book_data['id'])
     @books << book
   end
